03d2ad3336a284f6491f03524c5befd5994847c8
Danilo Argolo <dan_argolo@hotmail.com>
2023-03-14 03:46:21 -0300

    [FEAT] Query - Func. Agregação Inner Join

---
+++ challenges/desafio6.sql
@@ -0,0 +1,10 @@
+SELECT
+  MIN(p.valor_plano) AS faturamento_minimo,
+  MAX(p.valor_plano) AS faturamento_maximo,
+  ROUND(AVG(p.valor_plano), 2) AS faturamento_medio,
+  SUM(p.valor_plano) AS faturamento_total
+FROM
+  SpotifyClone.plano AS p
+    INNER JOIN
+  SpotifyClone.usuario AS u 
+ON p.id_plano = u.fk_id_plano;
